63afa45014071eeeb662221cd2163f99f788218c
Aleksandar Dimitrov <netbull2007@gmail.com>
2018-07-09 22:20:06 +0300

    Update

---
+++ Twig/LocaleSwitcherExtension.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace NetBull\CoreBundle\Twig;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use NetBull\CoreBundle\Locale\Switcher\TargetInformationBuilder;
+
+/**
+ * Class LocaleSwitcherExtension
+ * @package NetBull\CoreBundle\Twig
+ */
+class LocaleSwitcherExtension extends \Twig_Extension
+{
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * LocaleSwitcherExtension constructor.
+     * @param ContainerInterface $container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @return array The added functions
+     */
+    public function getFunctions()
+    {
+        return array(
+            new \Twig_SimpleFunction('locale_switcher', [$this, 'renderSwitcher'],['is_safe' => ['html']]),
+        );
+    }
+
+    /**
+     * @return string The name of the extension
+     */
+    public function getName()
+    {
+        return 'locale_switcher';
+    }
+
+    /**
+     * @param null $route
+     * @param array $parameters
+     * @param null $template
+     * @return mixed
+     * @throws \Exception
+     */
+    public function renderSwitcher($route = null, $parameters = array(), $template = null)
+    {
+        $showCurrentLocale = $this->container->getParameter('slr_locale.switcher.show_current_locale');
+        $useController = $this->container->getParameter('slr_locale.switcher.use_controller');
+        $allowedLocales = $this->container->get('slr_core.allowed_locales_provider')->getAllowedLocales();
+        $request = $this->container->get('request_stack')->getMasterRequest();
+        $router = $this->container->get('router');
+        $infoBuilder = new TargetInformationBuilder($request, $router, $allowedLocales, $showCurrentLocale, $useController);
+        $info = $infoBuilder->getTargetInformation($route, $parameters);
+
+        return $this->container->get('slr_core.locale_switcher_helper')->renderSwitch($info, $template);
+    }
+}
